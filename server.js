457179ae7556804ee14ee1644eedd9593cfc955c
JoaoPMV <joaop.trilha@gmail.com>
2026-08-07 00:15:39 -0300

    Initial backend commit

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options("/users/login", cors(corsOptions));
+app.options("/users/register", cors(corsOptions));
 
 app.use(express.json());
 
